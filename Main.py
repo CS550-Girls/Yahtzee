bc474de62878c014d62f5ba3afc6effbbe55fb4b
Annabelle Strong <annabellestrong@gmail.com>
2017-12-19 12:53:48 -0500

    Restored earlier version

--- Main.py
+++ Main.py
@@ -28,8 +28,8 @@ class Die():
 				elif self.save == True:
 					image = pygame.image.load('Resources/Die_' + str(self.value) + '_grey.png')
 
-					self.image =  pygame.transform.scale(image, (100, 100))
-					screen.blit(self.image, self.rect)
+				self.image =  pygame.transform.scale(image, (100, 100))
+				screen.blit(self.image, self.rect)
 
 				return True
 
@@ -118,21 +118,6 @@ class Dice():
 
 			self.rolls = 0
 
-	def toggle_status(self, num):
-	    Die = self.dice[num - 1]
-
-	    Die.save = not Die.save
-	    print(Die.save)
-
-	    if Die.save == False:
-	        image = pygame.image.load('Resources/Die_' + str(Die.value) + '.png')
-
-	    elif Die.save == True:
-	        image = pygame.image.load('Resources/Die_' + str(Die.value) + '_grey.png')
-
-	    Die.image =  pygame.transform.scale(image, (100, 100))
-	    screen.blit(Die.image, Die.rect)
-
 class Button():
   def __init__(self, rect, row):
       self.rect = rect
@@ -141,126 +126,131 @@ class Button():
       self.filled = False
 
   def calc(self):
-    global total
     global Dice
 
-    values = Dice.values
-
     if not self.filled:
-        if self.row <= 6:
-          self.score = self.row * values.count(self.row)
+	    if self.row <= 6:
+	      self.score = self.row * Dice.values.count(self.row)
 
-        elif self.row == 7:
-            for i in range(1, 7):
-                num = values.count(i)
-                if num == 3:
-                    self.score = sum(values)
-                    break
+	    elif self.row == 7:
+	        for i in range(1, 7):
+	            num = Dice.values.count(i)
+	            if num == 3:
+	                self.score = sum(Dice.values)
+	                break
 
-        elif self.row == 8:
-            for i in range(1, 7):
-                num = values.count(i)
-                if num == 4:
-                    self.score = sum(values)
-                    break
+	    elif self.row == 8:
+	        for i in range(1, 7):
+	            num = Dice.values.count(i)
+	            if num == 4:
+	                self.score = sum(Dice.values)
+	                break
 
-        elif self.row == 9:
-            taken = -1
-            two = False
-            three = False
+	    elif self.row == 9:
+	        taken = -1
+	        two = False
+	        three = False
 
-            for i in range(1, 7):
-                num = values.count(i)
-                if num == 3:
-                    taken = i
-                    three = True
-                    break
+	        for i in range(1, 7):
+	            num = Dice.values.count(i)
+	            if num == 3:
+	                taken = i
+	                three = True
+	                break
 
-            for i in range(1, 7):
-                if i != taken:
-                    num = values.count(i)
-                    if num == 2:
-                        two = True
-                        break
+	        for i in range(1, 7):
+	            if i != taken:
+	                num = Dice.values.count(i)
+	                if num == 2:
+	                    two = True
+	                    break
 
-            if two and three == True:
-                self.score = 25
+	        if two and three == True:
+	            self.score = 25
 
-        elif self.row == 10:
-            for i in range(0, len(values)):
-                for x in range(0, len(values)-1):
-                    for i in range(1, len(values)):
-                        a = values[i-1]
-                        b = values[i]
+	    elif self.row == 10:
+	        for i in range(0, len(Dice.values)):
+	            for x in range(0, len(Dice.values)-1):
+	                for i in range(1, len(Dice.values)):
+	                    a = Dice.values[i-1]
+	                    b = Dice.values[i]
 
-                        if a > b:
-                            values[i-1] = b
-                            values[i] = a
+	                    if a > b:
+	                        Dice.values[i-1] = b
+	                        Dice.values[i] = a
 
-            for x in range(3, 4):
-                a = values[i-3]
-                b = values[i-2]
-                c = values[i-1]
-                d = values[i]
+	        for i in range(3, 4):
+	            a = Dice.values[i-3]
+	            b = Dice.values[i-2]
+	            c = Dice.values[i-1]
+	            d = Dice.values[i]
 
-                if b == a+1 and c == a+2 and d == a+3:
-                    self.score = 30
-                    break
+	            if b == a+1 and c == a+2 and d == a+3:
+	                self.score = 30
+	                break
 
-        elif self.row == 11:
-            for i in range(0, len(values)):
-                for x in range(0, len(values)-1):
-                    for i in range(1, len(values)):
-                        a = values[i-1]
-                        b = values[i]
+	    elif self.row == 11:
+	        for i in range(0, len(Dice.values)):
+	            for x in range(0, len(Dice.values)):
+	                for i in range(1, len(Dice.values)):
+	                    a = Dice.values[i-1]
+	                    b = Dice.values[i]
 
-                        if a > b:
-                            values[i-1] = b
-                            values[i] = a
+	                    if a > b:
+	                        Dice.values[i-1] = b
+	                        Dice.values[i] = a
 
-            a = values[0]
-            b = values[1]
-            c = values[2]
-            d = values[3]
-            e = values[4]
+	        a = Dice.values[0]
+	        b = Dice.values[1]
+	        c = Dice.values[2]
+	        d = Dice.values[3]
+	        e = Dice.values[4]
 
-            if b == a+1 and c == a+2 and d == a+3 and e == a+4:
-                self.score = 40
+	        if b == a+1 and c == a+2 and d == a+3 and e == a+4:
+	            self.score = 40
 
-        elif self.row == 12:
-            for i in range(1, 7):
-                num = values.count(i)
-                if num == 5:
-                    self.score = 50
-                    break
+	    elif self.row == 12:
+	        a = Dice.values[0]
+	        b = Dice.values[1]
+	        c = Dice.values[2]
+	        d = Dice.values[3]
+	        e = Dice.values[4]
 
-        elif self.row == 13:
-            self.score = sum(values)
+	        if a == b == c == d == e:
+	            self.score = 50
+
+	    elif self.row == 13:
+	        self.score = sum(Dice.values)
 
   def check_status(self):
-     self.calc()
+	  global subscores
 
-     mouse = pygame.mouse.get_pos()
-     click = pygame.mouse.get_pressed()
+	  self.calc()
 
-     if (self.rect.x + 475) > mouse[0] > self.rect.x and (self.rect.y + 30) > mouse[1] > self.rect.y:
-         if click[0] == 1:
-             time.sleep(.001)
+	  mouse = pygame.mouse.get_pos()
+	  click = pygame.mouse.get_pressed()
 
-             print(self.score)
+	  if (self.rect.x + 475) > mouse[0] > self.rect.x and (self.rect.y + 30) > mouse[1] > self.rect.y:
+		  if click[0] == 1:
+			  time.sleep(.001)
 
-             self.filled = True
+			  print(self.score)
+			  subscores.append(self.score)
+
+			  self.filled = True
+
+	  return self.filled
 
   def print(self):
       button = pygame.Surface((self.rect.w, self.rect.h))
-      button.set_alpha(50)
+      button.set_alpha(150)
       button.fill((0, 128, 255))
       screen.blit(button, (self.rect.x, self.rect.y))
 
       if self.filled == True:
            small_font = pygame.font.SysFont('Calibri Light', 28)
            text = small_font.render(str(self.score), True, (0, 0, 0))
-           screen.blit(text, (270, self.rect.y + 5))
+           screen.blit(text, (1070, self.rect.y + 5))
 
 
 class Buttons():
@@ -292,10 +282,21 @@ class Buttons():
             Button.print()
 
     def check_status(self):
-        for i in range(0, len(self.buttons)):
-            Button = self.buttons[i]
+        self.clicked = []
+        print(self.clicked)
+        clicked = False
 
-            Button.check_status()
+        for i in range(0, len(self.buttons)):
+            if not i in self.clicked:
+	            Button = self.buttons[i]
+
+	            clicked = Button.check_status()
+
+	            if clicked == True:
+	                self.clicked.append(i)
+	                break
+
+        return clicked
 
 def screen_set():
     screen = pygame.display.set_mode((1375, 675))
@@ -304,7 +305,7 @@ def screen_set():
     background_image = pygame.transform.scale(pygame.image.load('Resources/Background.png'), (800, 700))
     screen.blit(background_image, (0, 0))
 
-    scorecard_background_image = pygame.transform.scale(pygame.image.load('Resources/Scoresheet.png'), (574, 673))
+    scorecard_background_image = pygame.image.load('Resources/Scoresheet.jpg')
     screen.blit(scorecard_background_image, (800, 0))
 
     if Dice.rolls < 3:
@@ -315,14 +316,6 @@ def screen_set():
         small_font = pygame.font.SysFont('Calibri Light', 28)
         text = small_font.render(str(3-Dice.rolls)+ ' rolls left', True, (255, 255, 255))
         screen.blit(text, (350, 550))
-
-        if tutorial == True and Dice.rolls >= 1:
-            smallest_font = pygame.font.SysFont('Calibri Light', 20)
-            text1 = smallest_font.render("Click on the dice you want to save", True, (255, 255, 255))
-            screen.blit(text1, (300, 420))
-            time.sleep(1)
-            text2 = smallest_font.render("When you're happy with your roll, click on a row on the scoreboard to the right to save your score", True, (255, 255, 255))
-            screen.blit(text2, (100, 600))
 
     Dice.print()
     Buttons.print()
@@ -342,7 +335,6 @@ pygame.display.set_icon(icon)
 done = False
 scored = False
 rolled = False
-tutorial = True
 subscores = []
 
 Buttons = Buttons()
@@ -363,26 +355,11 @@ while not done:
 			if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and Dice.rolls <= 3:
 				Dice.roll()
 				rolled = True
-			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_r:
-					Dice.reset()
-				elif event.key == pygame.K_1:
-					Dice.toggle_status(1)
-				elif event.key == pygame.K_2:
-					Dice.toggle_status(2)
-				elif event.key == pygame.K_3:
-					Dice.toggle_status(3)
-				elif event.key == pygame.K_4:
-					Dice.toggle_status(4)
-				elif event.key == pygame.K_5:
-					Dice.toggle_status(5)
-				elif event.key == pygame.K_6:
-					Dice.toggle_status(6)
-
-			'''
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+				Dice.reset()
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				Dice.check_status()
-			'''
+
 		pygame.display.flip()
 
 	for event in pygame.event.get():
@@ -390,30 +367,18 @@ while not done:
 			done = True
 		if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and Dice.rolls <= 3:
 			Dice.roll()
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_r:
-				Dice.reset()
-			elif event.key == pygame.K_1:
-				Dice.toggle_status(1)
-			elif event.key == pygame.K_2:
-				Dice.toggle_status(2)
-			elif event.key == pygame.K_3:
-				Dice.toggle_status(3)
-			elif event.key == pygame.K_4:
-				Dice.toggle_status(4)
-			elif event.key == pygame.K_5:
-				Dice.toggle_status(5)
-			elif event.key == pygame.K_6:
-				Dice.toggle_status(6)
-
+			rolled = True
+		if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+			Dice.reset()
 		if event.type == pygame.MOUSEBUTTONDOWN:
-			scored = Buttons.check_status()
+			if Dice.check_status() == False:
+				scored = Buttons.check_status()
+			print(scored)
 
 	if scored == True:
 		Dice.reset()
 		scored = False
 		rolled = False
-		tutorial = False
 		print(sum(subscores))
 
 	pygame.display.flip()
