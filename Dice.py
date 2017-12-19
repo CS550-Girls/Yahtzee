602b25f0265128dbd86e3a5b22d297c2120f8ca2
Annabelle Strong <annabellestrong@gmail.com>
2017-12-19 15:30:00 -0500

    Added comments

--- Dice.py
+++ Dice.py
@@ -4,62 +4,71 @@ import time
 
 class Die():
 	def __init__(self, rect):
-		self.rect = rect
-		self.value = 1
-		self.save = False
+		self.rect = rect # Define position
+		self.value = 1 # Set initial value
+		self.save = False # Set default save value (False = 'don't save')
 
+		# Load and print image corresponding to value
 		image = pygame.image.load('Resources/Die_' + str(self.value) + '.png')
 		self.image =  pygame.transform.scale(image, (100, 100))
 
 	def check_status(self):
+		# Get mouse status
 		mouse = pygame.mouse.get_pos()
 		click = pygame.mouse.get_pressed()
 
-		image = self.image
+		image = self.image # Set default image
 
 		if (self.rect.x + 100) > mouse[0] > self.rect.x and (self.rect.y + 100) > mouse[1] > self.rect.y:
 			if click[0] == 1:
-				self.save = not self.save
+				self.save = not self.save # Switch status
 
-				print('clicked!')
+				print('clicked!') # For testing
 
+				# Switch image to match save status
 				if self.save == False:
 					image = pygame.image.load('Resources/Die_' + str(self.value) + '.png')
 				elif self.save == True:
 					image = pygame.image.load('Resources/Die_' + str(self.value) + '_grey.png')
 
-				self.image =  pygame.transform.scale(image, (100, 100))
-				screen.blit(self.image, self.rect)
-				time.sleep(.001)
+		# Load and print image
+		self.image =  pygame.transform.scale(image, (100, 100))
+		screen.blit(self.image, self.rect)
+		time.sleep(.001)
 
 
 	def print(self):
-		screen.blit(self.image, self.rect)
+		screen.blit(self.image, self.rect) # Print image
 
 	def roll(self):
-		self.value = random.randint(1,6)
+		self.value = random.randint(1,6) # Generate random value
+
+		# Load and print new image corresponding to value
 		image = pygame.image.load('Resources/Die_' + str(self.value) + '.png')
 		self.image =  pygame.transform.scale(image, (100, 100))
+		screen.blit(self.image, self.rect)
 
-		self.print()
-
+		# Return value
 		return self.value
 
 	def reset(self):
-		self.save = False
-		self.value = 1
+		self.save = False # Un-save all dice
+		self.value = 1 # Reset to default value
+
+		# Load and print new image corresponding to value
 		image = pygame.image.load('Resources/Die_' + str(self.value) + '.png')
 		self.image =  pygame.transform.scale(image, (100, 100))
 		screen.blit(self.image, self.rect)
 
 class Dice():
 	def __init__(self):
-		self.score = 0
-		self.rolls = 0
+		self.score = 0 # Set starting score
+		self.rolls = 0 # Reset number of rolls
 
-		self.values = []
-		self.dice = []
+		self.values = [] # Create list for dice values in a given roll
+		self.dice = [] # Create list for dice in 'hand'
 
+		# Create dice
 		for i in range(0, 5):
 			x = 50 + (150*i)
 			y = 300
@@ -70,54 +79,45 @@ class Dice():
 	def print(self):
 		for i in range(0, len(self.dice)):
 			Die = self.dice[i]
-
 			Die.print()
 
 	def roll(self):
+		# Loop through dice and generate new values
 		for i in range(0, len(self.dice)):
 			Die = self.dice[i]
-
 			Die.check_status()
 
 			if Die.save !=  True:
 				self.values.append(Die.roll())
 
 			Die.print()
+
+		# Add one to roll count
 		self.rolls += 1
 
 	def check_status(self):
+		# Loop through dice and check if they're being clicked
 		for i in range(0, len(self.dice)):
 			Die = self.dice[i]
-
 			Die.check_status()
 
 	def reset(self):
+		# Loop through dice and reset each
 		for i in range(0, len(self.dice)):
 			Die = self.dice[i]
-
 			Die.reset()
 
-			screen.fill([255, 255, 255])
-
-			background_image = pygame.transform.scale(pygame.image.load('Resources/Background.png'), (800, 600))
-			screen.blit(background_image, (0, 0))
-
-			big_font = pygame.font.SysFont('Calibri Light', 35)
-			text = big_font.render('press the spacebar to roll', True, (255, 255, 255))
-			screen.blit(text, (250, 520))
-
-			small_font = pygame.font.SysFont('Calibri Light', 28)
-			text = small_font.render(str(3-Dice.rolls)+ ' rolls left', True, (255, 255, 255))
-			screen.blit(text, (350, 550))
-
-			self.rolls = 0
+		# Reset roll count
+		self.rolls = 0
 
 def screen_set():
+	# Print background visuals
 	screen.fill([255, 255, 255])
 
 	background_image = pygame.transform.scale(pygame.image.load('Resources/Background.png'), (800, 600))
 	screen.blit(background_image, (0, 0))
 
+	# Print instructional text if rolling is still active
 	if Dice.rolls < 3:
 		big_font = pygame.font.SysFont('Calibri Light', 35)
 		text = big_font.render('press the spacebar to roll', True, (255, 255, 255))
@@ -130,17 +130,20 @@ def screen_set():
 	Dice.print()
 
 ############## GAME LOGIC
+# Initialize pygame and its components
 pygame.init()
 pygame.font.init()
 
+# Set up window
 screen = pygame.display.set_mode((800, 600))
-
 pygame.display.set_caption("Yahtzee")
 icon = pygame.image.load('Resources/Icon.png')
 pygame.display.set_icon(icon)
 
+# Create instance of Dice
 Dice = Dice()
 
+# Set default 'done' bool value
 done = False
 
 while not done:
--- Scoreboard.py
+++ Scoreboard.py
@@ -4,33 +4,35 @@ import random
 
 class Button():
   def __init__(self, rect, row):
-      self.rect = rect
-      self.score = 0
-      self.row = row
-      self.filled = False
+      self.rect = rect # Define position
+      self.score = 0 # Set default score
+      self.row = row # Set row number on grid
+      self.filled = False # Set default 'filled' bool value
 
   def calc(self):
     global total
 
+    # Calculate score based on given values and row number
+
     if not self.filled:
-        if self.row <= 6:
+        if self.row <= 6: # Add all occurances of a given number
           self.score = self.row * values.count(self.row)
 
-        elif self.row == 7:
+        elif self.row == 7: # Add sum if a given number occurs three times
             for i in range(1, 7):
                 num = values.count(i)
                 if num == 3:
                     self.score = sum(values)
                     break
 
-        elif self.row == 8:
+        elif self.row == 8: # Add sum if a given number occurs four times
             for i in range(1, 7):
                 num = values.count(i)
                 if num == 4:
                     self.score = sum(values)
                     break
 
-        elif self.row == 9:
+        elif self.row == 9: # Set score to 25 if one number occurs twice and another occurs three times
             taken = -1
             two = False
             three = False
@@ -52,7 +54,7 @@ class Button():
             if two and three == True:
                 self.score = 25
 
-        elif self.row == 10:
+        elif self.row == 10: # Set score to 30 if 4 of the digits are in sequence
             for i in range(0, len(values)):
                 for x in range(0, len(values)-1):
                     for i in range(1, len(values)):
@@ -73,7 +75,7 @@ class Button():
                     self.score = 30
                     break
 
-        elif self.row == 11:
+        elif self.row == 11: # Set score to 40 if all 5 of the digits are in sequence
             for i in range(0, len(values)):
                 for x in range(0, len(values)-1):
                     for i in range(1, len(values)):
@@ -93,36 +95,39 @@ class Button():
             if b == a+1 and c == a+2 and d == a+3 and e == a+4:
                 self.score = 40
 
-        elif self.row == 12:
+        elif self.row == 12: # Set score to 50 if all 5 of the digits are the same
             for i in range(1, 7):
                 num = values.count(i)
                 if num == 5:
                     self.score = 50
                     break
 
-        elif self.row == 13:
+        elif self.row == 13: # Calculate sum of values
             self.score = sum(values)
 
   def check_status(self):
-     self.calc()
-
+     # Get mouse position
      mouse = pygame.mouse.get_pos()
      click = pygame.mouse.get_pressed()
 
      if (self.rect.x + 475) > mouse[0] > self.rect.x and (self.rect.y + 30) > mouse[1] > self.rect.y:
          if click[0] == 1:
-             time.sleep(.001)
+             self.calc() # Calculate score
 
              print(self.score)
 
-             self.filled = True
+             self.filled = True # Set status to 'filled'
+
+             time.sleep(.001)
 
   def print(self):
+      # Create invsible button rectangle
       button = pygame.Surface((self.rect.w, self.rect.h))
       button.set_alpha(0)
       button.fill((0, 128, 255))
       score_screen.blit(button, (self.rect.x, self.rect.y))
 
+      # Print score if button has been clicked
       if self.filled == True:
            small_font = pygame.font.SysFont('Calibri Light', 28)
            text = small_font.render(str(self.score), True, (0, 0, 0))
@@ -131,9 +136,9 @@ class Button():
 
 class Buttons():
     def __init__(self):
-        self.buttons = []
+        self.buttons = [] # Define list to hold buttons
 
-        for i in range(0, 6):
+        for i in range(0, 6): # Loop through and create top 6 buttons
             y = 108 + (28*i)
             x = 50
             rect = pygame.Rect(x, y, 474, 27)
@@ -142,7 +147,7 @@ class Buttons():
 
             self.buttons.append(Button(rect, row))
 
-        for i in range(6, 13):
+        for i in range(6, 13): # Loop through and create bottom 6 buttons
             y = 226 + (28*i)
             x = 50
             rect = pygame.Rect(x, y, 474, 27)
@@ -158,7 +163,7 @@ class Buttons():
             Button.print()
 
     def check_status(self):
-        for i in range(0, len(self.buttons)):
+        for i in range(0, len(self.buttons)): # Loop through and check status of buttons
             Button = self.buttons[i]
 
             Button.check_status()
@@ -174,32 +179,33 @@ def screen_set():
 def reset():
     global values
 
-    for i in range(0, len(values)):
+    for i in range(0, len(values)): # Reset roll-mimicing values
         values[i] = random.randint(1,6)
 
     print(values)
 
 
 ############## GAME LOGIC
+# Initialize pygame and its components
 pygame.init()
 pygame.font.init()
 
+# Set up window
 score_screen = pygame.display.set_mode((574, 673))
-
 pygame.display.set_caption("Yahtzee")
 icon = pygame.image.load('Resources/Icon.png')
 pygame.display.set_icon(icon)
 
+# Set default values
+reset()
 done = False
 scored = False
 values = [0,0,0,0,0]
 
+# Create instance of Dice
 Buttons = Buttons()
 
-reset()
-
 while not done:
-
     screen_set()
 
     pressed = pygame.key.get_pressed()
