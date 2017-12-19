47adec82abcb4e68685de4324cdca5543dd70b51
Annabelle Strong <annabellestrong@gmail.com>
2017-12-19 11:08:50 -0500

    Updated to make fit with structure of Main

--- Scoreboard.py
+++ Scoreboard.py
@@ -1,7 +1,6 @@
 import pygame
 import time
-
-values = [1,2,3,4,5]
+import random
 
 class Button():
   def __init__(self, rect, row):
@@ -13,95 +12,96 @@ class Button():
   def calc(self):
     global total
 
-    if self.row <= 6:
-      self.score = self.row * values.count(self.row)
+    if not self.filled:
+        if self.row <= 6:
+          self.score = self.row * values.count(self.row)
 
-    elif self.row == 7:
-        for i in range(1, 7):
-            num = values.count(i)
-            if num == 3:
-                self.score = sum(values)
-                break
-
-    elif self.row == 8:
-        for i in range(1, 7):
-            num = values.count(i)
-            if num == 4:
-                self.score = sum(values)
-                break
-
-    elif self.row == 9:
-        taken = -1
-        two = False
-        three = False
-
-        for i in range(1, 7):
-            num = values.count(i)
-            if num == 3:
-                taken = i
-                three = True
-                break
-
-        for i in range(1, 7):
-            if i != taken:
+        elif self.row == 7:
+            for i in range(1, 7):
                 num = values.count(i)
-                if num == 2:
-                    two = True
+                if num == 3:
+                    self.score = sum(values)
                     break
 
-        if two and three == True:
-            self.score = 25
+        elif self.row == 8:
+            for i in range(1, 7):
+                num = values.count(i)
+                if num == 4:
+                    self.score = sum(values)
+                    break
 
-    elif self.row == 10:
-        for i in range(0, len(values)):
-            for x in range(0, len(values)-1):
-                for i in range(1, len(values)):
-                    a = values[i-1]
-                    b = values[i]
+        elif self.row == 9:
+            taken = -1
+            two = False
+            three = False
 
-                    if a > b:
-                        values[i-1] = b
-                        values[i] = a
+            for i in range(1, 7):
+                num = values.count(i)
+                if num == 3:
+                    taken = i
+                    three = True
+                    break
 
-        for x in range(3, 4):
-            a = values[i-3]
-            b = values[i-2]
-            c = values[i-1]
-            d = values[i]
+            for i in range(1, 7):
+                if i != taken:
+                    num = values.count(i)
+                    if num == 2:
+                        two = True
+                        break
 
-            if b == a+1 and c == a+2 and d == a+3:
-                self.score = 30
-                break
+            if two and three == True:
+                self.score = 25
 
-    elif self.row == 11:
-        for i in range(0, len(values)):
-            for x in range(0, len(values)-1):
-                for i in range(1, len(values)):
-                    a = values[i-1]
-                    b = values[i]
+        elif self.row == 10:
+            for i in range(0, len(values)):
+                for x in range(0, len(values)-1):
+                    for i in range(1, len(values)):
+                        a = values[i-1]
+                        b = values[i]
 
-                    if a > b:
-                        values[i-1] = b
-                        values[i] = a
+                        if a > b:
+                            values[i-1] = b
+                            values[i] = a
 
-        a = values[0]
-        b = values[1]
-        c = values[2]
-        d = values[3]
-        e = values[4]
+            for x in range(3, 4):
+                a = values[i-3]
+                b = values[i-2]
+                c = values[i-1]
+                d = values[i]
 
-        if b == a+1 and c == a+2 and d == a+3 and e == a+4:
-            self.score = 40
+                if b == a+1 and c == a+2 and d == a+3:
+                    self.score = 30
+                    break
 
-    elif self.row == 12:
-        for i in range(1, 7):
-            num = values.count(i)
-            if num == 5:
-                self.score = 50
-                break
+        elif self.row == 11:
+            for i in range(0, len(values)):
+                for x in range(0, len(values)-1):
+                    for i in range(1, len(values)):
+                        a = values[i-1]
+                        b = values[i]
 
-    elif self.row == 13:
-        self.score = sum(values)
+                        if a > b:
+                            values[i-1] = b
+                            values[i] = a
+
+            a = values[0]
+            b = values[1]
+            c = values[2]
+            d = values[3]
+            e = values[4]
+
+            if b == a+1 and c == a+2 and d == a+3 and e == a+4:
+                self.score = 40
+
+        elif self.row == 12:
+            for i in range(1, 7):
+                num = values.count(i)
+                if num == 5:
+                    self.score = 50
+                    break
+
+        elif self.row == 13:
+            self.score = sum(values)
 
   def check_status(self):
      self.calc()
@@ -169,6 +169,17 @@ def screen_set():
 	background_image = pygame.image.load('Resources/Scoresheet.jpg')
 	score_screen.blit(background_image, (0, 0))
 
+	Buttons.print()
+
+def reset():
+    global values
+
+    for i in range(0, len(values)-1):
+        values[i] = random.randint(1,6)
+
+    print(values)
+
+
 ############## GAME LOGIC
 pygame.init()
 pygame.font.init()
@@ -181,20 +192,23 @@ pygame.display.set_icon(icon)
 
 done = False
 scored = False
+values = [0,0,0,0,0]
 
 Buttons = Buttons()
+
+reset()
 
 while not done:
 
     screen_set()
-
-    Buttons.print()
 
     pressed = pygame.key.get_pressed()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+        	reset()
         if event.type == pygame.MOUSEBUTTONDOWN:
             scored = Buttons.check_status()
 
